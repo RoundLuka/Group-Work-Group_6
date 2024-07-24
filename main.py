0fb482843cb181cca0a4d0fa790300311c7984f2
irakl1ikvinchia <iraklikvinchia8@gmail.com>
2024-07-24 17:11:00 +0300

    added a command

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
-    channel = client.get_channel(1264154912065982518)
+    channel = client.get_channel(1265012166180999311)
     await channel.send("Hello")
     print("Member joined successfuly welcomed")
 
 #leave
 @client.event
 async def on_member_leave(memeber):
-    channel = client.get_channel(1264154912065982518)
+    channel = client.get_channel(1265012166180999311)
     await channel.send("Goodbye")
     print("Member left, successfuly sent goodbye")
 
@@ -33,6 +33,18 @@ async def hello(ctx):
 @client.command()
 async def call(ctx):
     await ctx.send("Ring ring 🔔")
+
+@client.command()
+async def joke(ctx):
+    await ctx.send("kaci shedis saxinkleshi")
+
+@client.command()
+async def commands(ctx):
+    await ctx.send("these are the commands you can use: /hello, /call, /joke")
+
+@client.command()
+async def check(ctx):
+    await ctx.send("yes this code is correct!")
 
 
 
@@ -45,6 +57,7 @@ async def call(ctx):
 #/hello
 #/call
 #/joke
+#/check
 
 
 
